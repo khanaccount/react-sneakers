63499ceb2e16f6a8e076126b96e45a4cb9b4bb5c
khanaccount <98438218+khanaccount@users.noreply.github.com>
2023-05-02 20:47:56 +0700

    react-sneakers

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import AppContext from '../context'
+import React from "react";
+import AppContext from "../context";
 
 export const Info = ({ image, title, description }) => {
-  const { setCartOpened } = React.useContext(AppContext)
+	const { setCartOpened } = React.useContext(AppContext);
 
-  return (
-    <div className="cartEmpty d-flex align-center justify-center flex-column flex">
-      <img className="mb-20" width="120px" src={image} alt="empty-cart" />
-      <h2>{title}</h2>
-      <p className="opacity-6">{description}</p>
-      <button onClick={() => setCartOpened(false)} className="greenButton">
-        <img src="/img/arrow.svg" alt="Arrow" />
-        Вернуться назад
-      </button>
-    </div>
-  )
-}
+	return (
+		<div className="cartEmpty d-flex align-center justify-center flex-column flex">
+			<img className="mb-20" width="120px" src={image} alt="empty-cart" />
+			<h2>{title}</h2>
+			<p className="opacity-6">{description}</p>
+			<button onClick={() => setCartOpened(false)} className="greenButton">
+				<img src="/img/arrow.svg" alt="Arrow" />
+				Вернуться назад
+			</button>
+		</div>
+	);
+};
 
-export default Info
+export default Info;
--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -1,9 +1,9 @@
 import AppContext from "../context";
 import React from "react";
 
-export const useCart = () => {  
-    const { cartItems, setCartItems } = React.useContext(AppContext);
-    const totalPrice = cartItems.reduce((sum, obj) => obj.price + sum, 0)
+export const useCart = () => {
+	const { cartItems, setCartItems } = React.useContext(AppContext);
+	const totalPrice = cartItems.reduce((sum, obj) => obj.price + sum, 0);
 
-    return { cartItems, setCartItems, totalPrice};
-}
+	return { cartItems, setCartItems, totalPrice };
+};
